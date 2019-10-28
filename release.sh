6d048b5f4ccea6d24c48b6854673d3f03498a2f9
nicholas.dickey <nicholas.dickey@hudsonwilde.com>
2019-10-28 12:25:14 -0400

    added release.sh

--- release.sh
+++ release.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 git pull
 echo "Copy /next"
-scp -r nick@dev.qwiket.com:/var/www/q2020/sites/qwiket/.next/* /var/www/q2020/sites/qwiket/.next
+scp -r nick@dev.qwiket.com:/home/nick/q2020/sites/qwiket/.next/* /var/www/q2020/sites/qwiket/.next
 pm2 restart qwiket-2020
