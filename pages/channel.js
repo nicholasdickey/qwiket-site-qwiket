1f9d9d795bbf92f74c08eca2f0c280754de82b39
nicholas.dickey <nicholas.dickey@hudsonwilde.com>
2019-10-31 10:02:00 -0400

    added channel page

---
+++ pages/channel.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { bindActionCreators } from 'redux'
+import Root from 'window-or-global'
+import { connect } from 'react-redux'
+import { fetchPageData } from '../qwiket-lib/actions/app'
+
+class Channel extends React.Component {
+    static async getInitialProps({ store, isServer, req, query }) {
+        if (isServer && req) {
+            Root.host = req.headers.host;
+        }
+        let { channel, q, solo, code } = query;
+        //  console.log({ store, isServer, query })
+        await store.dispatch(fetchPageData({ ssrType: 'channel', channel, ssrQid: q, code }));
+        console.log("AFTER DISPATCH")
+        return { query }
+    }
+    /*
+        componentDidMount() {
+            this.timer = this.props.startClock()
+        }
+    
+        componentWillUnmount() {
+            clearInterval(this.timer)
+        }*/
+
+    render() {
+        const { app } = this.props;
+        console.log("RENDER CHANNEL:")
+        return <div>CHANNEL:<div>{JSON.stringify(app, null, 4)}</div></div>
+    }
+}
+function mapStateToProps(state) {
+    return {
+        app: state.app,
+    };
+}
+const mapDispatchToProps = dispatch => {
+    return {
+        fetchPageData: bindActionCreators(fetchPageData, dispatch),
+
+    }
+}
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(Channel)
