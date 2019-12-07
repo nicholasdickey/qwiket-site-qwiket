784140d94ff19ee66825e781a9a67480edc4f026
nicholas.dickey <nicholas.dickey@hudsonwilde.com>
2019-12-07 08:10:36 -0500

    deploy

--- pages/_document.js
+++ pages/_document.js
@@ -1,11 +1,11 @@
-
+import "core-js";
+import regeneratorRuntime from "regenerator-runtime";
 import React from 'react';
 import Document, { Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheet } from 'styled-components'
 import { ServerStyleSheets } from '@material-ui/styles';
 import theme from '../views/theme';
-import "core-js";
-import regeneratorRuntime from "regenerator-runtime";
+
 class MyDocument extends Document {
     static async getInitialProps(ctx) {
         const styledComponentsSheet = new ServerStyleSheet()
